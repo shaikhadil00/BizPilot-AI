82a1d95f8ba8d1ee684eda4104b29555591e65bb
shaikhadil00 <128841756+shaikhadil00@users.noreply.github.com>
2026-07-25 01:30:50 +0530

    Added Streamlit Secrets support

--- core/config.py
+++ core/config.py
@@ -1,9 +1,14 @@
 import os
+import streamlit as st
 from dotenv import load_dotenv
 
 load_dotenv()
 
-GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+# First try Streamlit Secrets
+if "GROQ_API_KEY" in st.secrets:
+    GROQ_API_KEY = st.secrets["GROQ_API_KEY"]
+else:
+    GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 
 EMBEDDING_MODEL = "sentence-transformers/all-MiniLM-L6-v2"
 
